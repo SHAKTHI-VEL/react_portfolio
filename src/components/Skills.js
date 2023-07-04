77c7bf53c83d18f7d5acb00407083b009e98733b
SHAKTHI-VEL <shakthivel28203@gmail.com>
2023-07-04 13:20:37 +0530

    skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,7 +15,7 @@ function Skills() {
         <div className='text-4xl text-[#141C3A] text-bold ml-8 md:text-6xl mb-5'>
             <div className=''>Skills</div>
         </div>
-        <div className='flex flex-wrap justify-between'>
+        <div className='flex flex-wrap  justify-evenly'>
         <div className='w-[20%] ml-[10%] md:w-[10%] ml-[10%]' data-aos="fade-up"  data-aos-delay="100">
         <img src={require('../assets/html.png')} alt=""/>
         </div>
@@ -28,16 +28,16 @@ function Skills() {
         <div className='w-[20%] ml-[10%] mr-[10%] mt-3 md:w-[10%] ml-[10%] mr-[10%]' data-aos="fade-up"  data-aos-delay="100">
         <img src={require('../assets/mongodb.png')} alt=""/>
         </div>
-        <div className='w-[20%] ml-[3%] md:w-[10%] ml-[11%] mt-5 ' data-aos="fade-up"  data-aos-delay="100">
+        <div className='w-[20%] ml-[3%] md:w-[10%] ml-24 mt-5 ' data-aos="fade-up"  data-aos-delay="100">
         <img src={require('../assets/cplus.png')} alt=""/>
         </div>
-        <div className='w-[20%] ml-[10%] md:w-[10%] ml-[1%] mt-5' data-aos="fade-up"  data-aos-delay="100">
+        <div className='w-[20%] ml-[10%] md:w-[10%] ml-[80px] mt-5' data-aos="fade-up"  data-aos-delay="100">
         <img src={require('../assets/react.png')} alt=""/>
         </div>
-        <div className='w-[20%] ml-[10%] md:w-[10%] ml-[1%] mt-5' data-aos="fade-up"  data-aos-delay="100">
+        <div className='w-[20%] ml-[10%] md:w-[10%] mr-8 ml-5 mt-5' data-aos="fade-up"  data-aos-delay="100">
         <img src={require('../assets/github.png')} alt=""/>
         </div>
-        <div className='w-[20%] mr-2  md:w-[10%]  mt-5 mr-32' data-aos="fade-up"  data-aos-delay="100">
+        <div className='w-[20%] mr-2  md:w-[10%]  mt-5 mr-32 ml-2 ' data-aos="fade-up"  data-aos-delay="100">
         <img src={require('../assets/api.png')} alt=""/>
         </div>
         
